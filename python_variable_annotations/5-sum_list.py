6e9a4a0826440e20d6d8ba645ef0b04ed720a1e5
nathalie <nathalie.metreau@hotmail.fr>
2024-01-02 17:38:18 +0100

    correct t5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """def sum_list"""
+from typing import List
 
 
-def sum_list(input_list: [float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """takes a float n as argument and returns the string representation of
     the float."""
     return sum(input_list)
