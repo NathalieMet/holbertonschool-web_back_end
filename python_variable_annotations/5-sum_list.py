95e51e2af0b4b8dc6ada2edf5fe6848cb4d6d88f
nathalie <nathalie.metreau@hotmail.fr>
2024-01-02 16:02:09 +0100

    add task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""def sum_list"""
+
+
+def sum_list(input_list: [float]) -> float:
+    """takes a float n as argument and returns the string representation of
+    the float."""
+    return sum(input_list)
