4aba30473ef709967062ccb8310753124c5e36da
nathalie <nathalie.metreau@hotmail.fr>
2024-01-03 14:46:19 +0100

    correct t1

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,5 +8,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous coroutine that waits for a random delay and returns it."""
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    delays = []
+
+    async def helper():
+        """Helper function to gather the results."""
+        nonlocal delays
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    await asyncio.gather(*[helper() for _ in range(n)])
+
     return sorted(delays)
