093a2979e137393cd8e27d1e732d479794ffd79d
nathalie <nathalie.metreau@hotmail.fr>
2024-01-03 11:35:44 +0100

    correct t7 switch int and float

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes a string k and an int OR float v as arguments and returns a
     tuple."""
     return k, float(v * v)
