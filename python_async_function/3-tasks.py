b7a4cad4c309687b1c395fcf85ac1adcdfc15b7a
nathalie <nathalie.metreau@hotmail.fr>
2024-01-03 15:34:06 +0100

    try correction on t3 and push t4

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,10 +2,9 @@
 """def task_wait_random"""
 
 import asyncio
-from typing import Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """returns a asyncio.Task."""
     return asyncio.create_task(wait_random(max_delay))
