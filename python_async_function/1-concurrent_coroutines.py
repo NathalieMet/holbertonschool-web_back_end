57112ef353d7ba3c30ab61d07bb5d1802768dbb2
nathalie <nathalie.metreau@hotmail.fr>
2024-01-04 10:21:22 +0100

    correct coms on task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-"""def wait_n"""
+"""def wait_n
+Import wait_random from the previous python file that you've written and write
+an async routine called wait_n that takes in 2 int arguments (in this order):
+n and max_delay. You will spawn wait_random n times with the specified
+max_delay.
+wait_n should return the list of all the delays (float values). The list of
+the delays should be in ascending order without using sort() because of
+concurrency.
+"""
 
 import asyncio
 from typing import List
@@ -7,6 +15,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Asynchronous coroutine that waits for a random delay and returns it."""
+    """Asynchronous coroutine that waits for a random delay and returns it.
+    Import wait_random from the previous python file that you've written
+    and write an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random n times with
+    the specified max_delay.
+    wait_n should return the list of all the delays (float values). The list of
+    the delays should be in ascending order without using sort() because of
+    oncurrency."""
+
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     return sorted(delays)
