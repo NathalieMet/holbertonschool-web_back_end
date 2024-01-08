36aa5e7932008d21a1b99bf4be62fa62165b59b0
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-08 16:50:10 +0100

    add task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Write a Python function that changes all topics of a
+school document based on the name:
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Write a Python function that changes all topics of a
+    school document based on the name:"""
+    result = mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topic": topics}})
+    return result
