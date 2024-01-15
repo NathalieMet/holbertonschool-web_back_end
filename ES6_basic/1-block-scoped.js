ec04fe90a1df8e8e2ef7f26a7e86826d298ca58d
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-15 13:05:35 +0100

    correct t1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,5 @@
-export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-
-    if (trueOrFalse) {
-        // eslint-disable-next-line no-unused-vars
-        let task = true;
-        // eslint-disable-next-line no-unused-vars
-        let task2 = false;
-    }
-
-    return [task, task2];
-    }
+export default function taskBlock() {
+	const task = false;
+	const task2 = true;
+	return [task, task2];
+	}
