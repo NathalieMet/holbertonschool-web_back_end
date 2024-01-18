806e83c60516821ea3dc51c9d42e2349e42ff258
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-18 11:30:45 +0100

    add task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,32 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    } else {
+      this._floors = floors;
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    } else {
+      this._floors = newFloors;
+    }
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
