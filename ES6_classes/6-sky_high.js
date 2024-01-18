31a7dada81ef331609bafee059186222d808772c
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-18 11:34:42 +0100

    correct task 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,11 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    } else {
-      this._floors = floors;
-    }
+    this._floors = floors;
   }
 
   get sqft() {
@@ -19,11 +15,7 @@ export default class SkyHighBuilding extends Building {
   }
 
   set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    } else {
-      this._floors = newFloors;
-    }
+    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
