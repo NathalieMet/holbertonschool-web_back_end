d9d6f5201a4df0a0e343426ff84fe9979f1f188f
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-26 12:47:01 +0100

    correct t6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  return Promise.allSettled(promises)
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })))
-    .catch((error) => {
-      console.error('Error in handleProfileSignup:', error);
-      return [];
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+  const resultArray = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    result.push({
+      status: 'rejected',
+      value: error.toString(),
     });
-}
+  }
+  return resultArray;
+};
+
+export default handleProfileSignup;
