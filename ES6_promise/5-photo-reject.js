4fdeec05765b0e70e6546196e293fdd274cdbcc0
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-26 12:07:18 +0100

    correct t5

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,3 +1,4 @@
 export default function uploadPhoto(filename) {
-	return Promise.reject(`Error: ${filename} cannot be processed`);
-  }
+  const error = new Error(`Error: ${filename} cannot be processed`);
+  return Promise.reject(error);
+}
