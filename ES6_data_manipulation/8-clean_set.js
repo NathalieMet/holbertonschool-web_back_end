79e10a25b08d176394452beabb24e3a8771484a1
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-29 15:21:11 +0100

    add task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+	if (startString == '') {
+		return ''
+	}
+	const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+    return filteredValues.join('-');
+}
