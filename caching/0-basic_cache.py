2727742055b41382f9bdac96b60be9f1d9c7caa2
nathalie <nathalie.metreau@hotmail.fr>
2024-05-27 13:04:39 +0200

    add task 0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" class BasicCache
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ class BasicCache that
+    inherits from BaseCaching
+    and is a caching system:
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        self.cache_data[key] = item
+
+        if key is None or item is None:
+            return
+
+    def get(self, key):
+        """ Get an item by key
+        """
+
+        if key is None or key not in self.cache_data:
+            return None
+
+        return (self.cache_data.get(key))
