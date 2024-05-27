24e9d33df110b99363283043457a93d94c0102c6
nathalie <nathalie.metreau@hotmail.fr>
2024-05-27 14:08:59 +0200

    correct task 1 removing space

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -27,7 +27,7 @@ class FIFOCache(BaseCaching):
         elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             discarded_key = self.order.pop(0)
             del self.cache_data[discarded_key]
-            print("DISCARD: ", discarded_key)
+            print("DISCARD:", discarded_key)
 
         self.cache_data[key] = item
         self.order.append(key)
