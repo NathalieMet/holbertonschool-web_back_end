986a3ed26dbaa4e107f068b615f389dc785cff8e
nathalie <nathalie.metreau@hotmail.fr>
2024-06-17 13:16:07 +0200

    correct documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,7 +7,18 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-
+    """Write a function called filter_datum that returns the log message
+    obfuscated:
+Arguments:
+fields: a list of strings representing all fields to obfuscate
+redaction: a string representing by what the field will be obfuscated
+message: a string representing the log line
+separator: a string representing by which character is separating all fields
+in the log line (message)
+The function should use a regex to replace occurrences of certain field values.
+filter_datum should be less than 5 lines long and use re.sub to perform the
+substitution with a single regex.
+"""
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}",
                   message)
