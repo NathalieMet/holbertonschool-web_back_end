6d36da027b78eb65520270093ac1a10eb6cfbbb9
nathalie <nathalie.metreau@hotmail.fr>
2024-06-17 13:21:45 +0200

    try correction on t0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,9 +4,11 @@ function called filter_datum that returns the log message obfuscated
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Write a function called filter_datum that returns the log message
     obfuscated:
 Arguments:
