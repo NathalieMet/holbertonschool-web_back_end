4ee74e8c8e9943667ad32833e0fcfda3f4645c9e
nathalie <nathalie.metreau@hotmail.fr>
2024-06-18 12:25:17 +0200

    add task 5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Implement a hash_password function that expects one string argument name
+password and returns a salted, hashed password, which is a byte string.
+
+Use the bcrypt package to perform the hashing (with hashpw).
+"""
+import bcrypt
+
+def hash_password(password) :
+    """ Implement a hash_password function that expects one string argument
+    name password and returns a salted, hashed password, which is a byte
+    string.
+        """
+    password_bytes = password.encode('utf-8')
+    hashed = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed
