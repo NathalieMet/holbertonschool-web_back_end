41fd4197aa740eac2991d05014acbdc0b292f9b4
nathalie <nathalie.metreau@hotmail.fr>
2024-06-19 15:47:33 +0200

    add task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Class to manage the API authentication
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+User = TypeVar('User')
+
+
+class Auth():
+    """ Class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ function require_auth
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ function authorization_header
+        """
+        return None
+
+
+    def current_user(self, request=None) -> User:
+        """ function current_user
+        """
+        return None
