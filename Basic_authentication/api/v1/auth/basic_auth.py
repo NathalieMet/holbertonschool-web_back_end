e778c78508188d7c82060d0ea9108d8d309f402c
nathalie <nathalie.metreau@hotmail.fr>
2024-06-19 17:47:08 +0200

    add task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ from .auth import Auth
 class BasicAuth(Auth):
     """ Class to manage the API authentication
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        else:
+            if authorization_header.startswith("Basic "):
+                return authorization_header[6:]
+            else:
+                return authorization_header[5:]
