4ab8ffe684ae76ff43cc43ca4d4a7c103ba7f059
nathalie <nathalie.metreau@hotmail.fr>
2024-06-19 17:33:06 +0200

    add task 6

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Class to manage the API authentication
+"""
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Class to manage the API authentication
+    """
+    pass
