f27049ceec92298ce34c0283da7ee76e11cb9187
nathalie <nathalie.metreau@hotmail.fr>
2024-06-20 14:02:05 +0200

    add task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,8 @@ Class to manage the API authentication
 import base64
 
 from .auth import Auth
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -64,3 +65,23 @@ class BasicAuth(Auth):
         password = decoded_base64_authorization_header[separator + 1:]
 
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ returns the User instance based on his email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
