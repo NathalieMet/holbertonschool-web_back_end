f59dae7250fdb90d7d971e8929acdf4f63abc395
nathalie <nathalie.metreau@hotmail.fr>
2024-06-21 16:05:37 +0200

    correct task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -59,20 +59,16 @@ def filter_request() -> str:
                                             '/api/v1/forbidden/',
                                             '/api/v1/auth_session/login/']):
         return
-    if auth.authorization_header(request) is None:
+
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
     request.current_user = auth.current_user(request)
 
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.session_cookie(request) is None:
-            abort(401)
-
     if request.current_user is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
