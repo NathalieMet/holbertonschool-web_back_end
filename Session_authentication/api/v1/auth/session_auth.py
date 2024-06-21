c61d4ab366f70384e6a5fcc9644ab35ad0046806
nathalie <nathalie.metreau@hotmail.fr>
2024-06-21 11:03:36 +0200

    add task 1

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Class to manage the API authentication
+"""
+
+import base64
+
+from .auth import Auth
+from typing import Tuple, TypeVar
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Class to manage the API authentication
+    """
+    pass
