be802f61fd59315971eb1e6867bb5939dbb0300c
nathalie <nathalie.metreau@hotmail.fr>
2024-06-22 11:25:56 +0200

    correct pycodestyle

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -7,6 +7,7 @@ from flask import abort, jsonify, request
 from models.user import User
 import os
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def session_login():
     """ POST /auth_session/login
@@ -17,19 +18,19 @@ def session_login():
     password = request.form.get('password')
 
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
 
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
 
     users = User.search({'email': email})
 
     if not users:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
 
     user = users[0]
     if not user.is_valid_password(password):
-            return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
 
