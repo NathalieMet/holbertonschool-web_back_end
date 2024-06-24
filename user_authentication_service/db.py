2a963f65fb9a45b9cbebdd4bd20229be6fec1127
nathalie <nathalie.metreau@hotmail.fr>
2024-06-24 14:10:59 +0200

    try correction

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,10 +31,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str, **kwargs) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database
         """
-        new_user = User(email=email, hashed_password=hashed_password, **kwargs)
+        new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
 
