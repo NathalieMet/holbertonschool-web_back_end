dc5d8bf16365390d7a8f701b67a7b7f482a688d3
nathalie <nathalie.metreau@hotmail.fr>
2024-06-24 15:48:06 +0200

    try correction

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
 
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """ takes in arbitrary keyword arguments and returns the first row
         found in the users table as filtered by the method’s input arguments
         """
