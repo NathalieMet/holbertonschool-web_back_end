903b89fbee772ea1dccaf220b21970e965f061c9
nathalie <nathalie.metreau@hotmail.fr>
2024-06-24 15:13:05 +0200

    try correction

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -50,7 +50,7 @@ class DB:
             user = self._session.query(User).filter_by(**kwargs).first()
 
             if user is None:
-                raise NoResultFound("No user found with the given parameters")
+                raise NoResultFound()
 
             return user
 
