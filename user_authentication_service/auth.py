35a7a6b47734497a6785eeeb048ef66d4c713c94
nathalie <nathalie.metreau@hotmail.fr>
2024-06-26 13:54:38 +0200

    add task 9

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from db import DB
 from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -62,3 +63,8 @@ class Auth:
             return False
         except InvalidRequestError:
             return False
+
+    def _generate_uuid() -> str:
+        """Return a string representation of a new UUID.
+        """
+        return str(uuid.uuid4())
