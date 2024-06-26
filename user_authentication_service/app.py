41700b167d711a40a1a796c2e962ff97e4bbfbee
nathalie <nathalie.metreau@hotmail.fr>
2024-06-26 12:57:46 +0200

    add task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,10 +3,14 @@
 Route module for the API
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from db import DB
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -19,5 +23,27 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """ POST /users/
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+      - 400 if can't create the new User
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        return jsonify({"message": "email and password required"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
