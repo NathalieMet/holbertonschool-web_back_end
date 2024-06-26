3cda6b8b30ddbee5301f013117672425dacf3968
nathalie <nathalie.metreau@hotmail.fr>
2024-06-26 14:04:44 +0200

    correct t9

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -26,6 +26,12 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """Return a string representation of a new UUID.
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -63,8 +69,3 @@ class Auth:
             return False
         except InvalidRequestError:
             return False
-
-    def _generate_uuid() -> str:
-        """Return a string representation of a new UUID.
-        """
-        return str(uuid.uuid4())
