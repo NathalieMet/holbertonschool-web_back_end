5418b3f5c5d85ab3eec584a1dd8bcff53181c2e9
nathalie <nathalie.metreau@hotmail.fr>
2024-06-26 13:33:46 +0200

    ADD TASK 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
@@ -34,7 +35,7 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """hash the password, save the user to the database
         and return the User object
-    """
+        """
         try:
             user = self._db.find_user_by(email=email)
             if user:
@@ -45,3 +46,19 @@ class Auth:
         hashed_password = _hash_password(password)
         new_user = self._db.add_user(email, hashed_password)
         return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check a password with bcrypt.checkpw. If it matches return True.
+        In any other case, return False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'),
+                              user.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+        except InvalidRequestError:
+            return False
