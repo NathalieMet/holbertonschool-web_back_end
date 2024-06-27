f92b28ad5cce2f89db97f9e0a85467b953b8689c
nathalie <nathalie.metreau@hotmail.fr>
2024-06-27 17:23:16 +0200

    correct style

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -130,8 +130,8 @@ def get_reset_password_token() -> str:
         abort(403)
 
     try:
-      new_token = AUTH.get_reset_password_token(email)
-      return jsonify({"email": email, "reset_token": new_token}), 200
+        new_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": new_token}), 200
 
     except ValueError:
         abort(403)
