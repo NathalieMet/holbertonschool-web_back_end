d2102175d1fd712cd2d0187bfe5f0be46309fbea
nathalie <nathalie.metreau@hotmail.fr>
2024-07-03 12:23:30 +0200

    ADD TASK 5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -39,6 +39,29 @@ class TestGithubOrgClient(unittest.TestCase):
         # Assert that the return value is the mocked return value
         self.assertEqual(org_data, {"some_key": "some_value"})
 
+    def test_public_repos_url(self):
+        """Test the _public_repos_url property"""
+
+        # Payload to be returned by the mocked `org` method
+        mock_payload = {"repos_url":
+                        "https://api.github.com/orgs/google/repos"}
+
+        # Use patch as a context manager to mock `GithubOrgClient.org`
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=property) as mock_org:
+            # Set the return value of the mocked property
+            mock_org.return_value = mock_payload
+
+            # Create an instance of GithubOrgClient
+            client = GithubOrgClient("google")
+
+            # Call the _public_repos_url property
+            result = client._public_repos_url
+
+            # Assert that the _public_repos_url returns the expected URL
+            self.assertEqual(result,
+                             "https://api.github.com/orgs/google/repos")
+
 
 if __name__ == "__main__":
     unittest.main()
