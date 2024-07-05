e0d8bc2d05e2973c642db8f71e4f0653be9bbcd8
nathalie <nathalie.metreau@hotmail.fr>
2024-07-05 14:31:18 +0200

    add task 4

---
+++ i18n/4-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+from flask import request
+
+
+def get_locale():
+    """
+    Determine the best match with our supported languages.
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def output_index():
+    """
+    route that simply outputs “Welcome to Holberton” as page title (<title>)
+    and “Hello world” as header (<h1>).
+    """
+    return render_template('4-index.html')
+
+
+class Config:
+    """
+    Config class for flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+app.run()
