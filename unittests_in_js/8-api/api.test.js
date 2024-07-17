58899a1b4034e9b91a828655c1799bf2e16740df
nathalie <nathalie.metreau@hotmail.fr>
2024-07-17 10:09:13 +0200

    try use request on t8

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,21 +1,20 @@
-const request = require('supertest');
-const app = require('./api');
+const request = require('request');
 const { expect } = require('chai');
+const app = require('./api');
 
 describe('Index Page', () => {
   it('should return status code 200', (done) => {
-    request(app)
-      .get('/')
-      .expect(200, done);
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
   it('should return the correct result', (done) => {
-    request(app)
-      .get('/')
-      .expect('Content-Type', /text/)
-      .expect((res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-      })
-      .end(done);
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
+
