0f87aed01f080c326f55ac7ccaad9e9117f709be
nathalie <nathalie.metreau@hotmail.fr>
2024-08-05 12:35:42 +0200

    add task 0

---
+++ 0x0B_redis_basic/README.md
@@ -0,0 +1,4 @@
+# holbertonschool-web_back_end
+Learning Objectives
+Learn how to use redis for basic operations
+Learn how to use redis as a simple cache
---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Exercise file
+"""
+import redis
+import uuid
+
+
+class Cache():
+    """The class Cache
+    """
+
+    def __init__(self):
+        """ Initialize the class """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """ Generate a random key (e.g. using uuid), store the input data in
+        Redis sing the random key and return the key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
