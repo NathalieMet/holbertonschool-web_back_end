947c28d1fceec8c1b8b6b7d832c4ee1f2574ab4b
nathalie <nathalie.metreau@hotmail.fr>
2024-08-05 13:29:54 +0200

    add task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Exercise file
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -23,3 +23,26 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None):
+        """ convert the data back to the desired format
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key):
+        """ parametrize Cache.get with the correct conversion function for str
+        """
+        data = self.get(key, lambda x: x.decode('utf-8') if x else None)
+        return data
+
+    def get_int(self, key):
+        """ parametrize Cache.get with the correct conversion function for int
+        """
+        data = self.get(key, lambda x: int(x) if x else None)
+        return data
+
