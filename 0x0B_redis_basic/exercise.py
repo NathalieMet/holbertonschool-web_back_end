95d8f2301ac849325566dbf593822bc614693253
nathalie <nathalie.metreau@hotmail.fr>
2024-08-05 14:18:51 +0200

    add task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,6 +5,18 @@ Exercise file
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count calls to a method.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function to count method calls."""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -17,6 +29,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generate a random key (e.g. using uuid), store the input data in
         Redis sing the random key and return the key """
@@ -45,4 +58,3 @@ class Cache():
         """
         data = self.get(key, lambda x: int(x) if x else None)
         return data
-
