74f6dcd5b088abc4a60c9aec719871535c58e7de
nathalie <nathalie.metreau@hotmail.fr>
2024-08-06 14:46:35 +0200

    add task 5

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- script that creates a trigger that resets the attribute valid_email only when the email has been changed
+
+DROP TRIGGER IF EXISTS reset_valid_email;
+
+DELIMITER //
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF OLD.email <> NEW.email THEN
+        SET NEW.valid_email = FALSE;
+    END IF;
+END; //
+
+DELIMITER ;
